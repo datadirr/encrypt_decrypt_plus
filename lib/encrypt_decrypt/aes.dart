13d5fefa12abb2a4475e1a7bf1db53161fee34f1
datadirr <datadirr@gmail.com>
2025-01-21 21:18:12 +0530

    * AES-256-CBC for encryption and decryption.

--- lib/encrypt_decrypt/aes.dart
+++ lib/encrypt_decrypt/aes.dart
@@ -10,13 +10,19 @@ class AES {
   /// [iv] 16 character long
   String iv;
 
+  /// [padding] default 0, any character
+  String padding;
+
   late aes.Key _secretKey;
   late aes.IV _iv;
 
-  AES({required this.secretKey, required this.iv}) {
+  AES({required this.secretKey, required this.iv, this.padding = "0"}) {
     try {
-      _secretKey = aes.Key.fromUtf8(secretKey);
-      _iv = aes.IV.fromUtf8(iv);
+      String secretKey32Bytes =
+          secretKey.padRight(32, padding).substring(0, 32);
+      String iv16Bytes = iv.padRight(16, padding).substring(0, 16);
+      _secretKey = aes.Key.fromUtf8(secretKey32Bytes);
+      _iv = aes.IV.fromUtf8(iv16Bytes);
     } catch (e) {
       log(e.toString());
     }
